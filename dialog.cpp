29b2e78878b11f5abed7dbd5cd17933d12274baf
flam1ngo <kris.s.03@mail.ru>
2024-10-15 21:57:52 +0300

    Second commit

---
+++ dialog.cpp
@@ -0,0 +1,26 @@
+#include "dialog.h"
+#include "ui_dialog.h"
+
+Dialog::Dialog(QWidget *parent) :
+    QDialog(parent),
+    ui(new Ui::Dialog)
+{
+    ui->setupUi(this);
+    ui->setupUi(this);
+    ui->label->setStyleSheet("color : Black");
+    ui->label_2->setStyleSheet("color : Black");
+    ui->label_3->setStyleSheet("color : Black");
+    ui->label_4->setStyleSheet("color : Black");
+    ui->label_5->setStyleSheet("color : Black");
+}
+
+Dialog::~Dialog()
+{
+    delete ui;
+}
+
+void Dialog::on_pushButton_clicked()
+{
+     this->close();
+}
+
--- mainwindow.cpp
+++ mainwindow.cpp
@@ -5,21 +5,23 @@ MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
-    ui->setupUi(this);
-    ui->centralwidget->setStyleSheet(" background-color: White");
-    ui->label->setStyleSheet("color : Black");
-    ui->label_2->setStyleSheet("color : Black");
-    ui->label_3->setStyleSheet("color : Black");
-    ui->label_4->setStyleSheet("color : Black");
-    ui->label_5->setStyleSheet("color : Black");
-    ui->pushButton->setStyleSheet("color : Green");
-    ui->pushButton_2->setStyleSheet("color : Red");
+
+ui->setupUi(this);
+
 
 
 }
 
+
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+
+void MainWindow::on_pushButton_clicked()
+{
+    d = new Dialog(this);
+    d->show();
 }
 
