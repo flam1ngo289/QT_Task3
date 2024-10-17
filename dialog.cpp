5d30dc3b424d048647222aeed34065e6ec0fa525
flam1ngo <kris.s.03@mail.ru>
2024-10-17 20:03:37 +0300

    Third commit

--- dialog.cpp
+++ dialog.cpp
@@ -5,8 +5,8 @@ Dialog::Dialog(QWidget *parent) :
     QDialog(parent),
     ui(new Ui::Dialog)
 {
-    ui->setupUi(this);
-    ui->setupUi(this);
+   ui->setupUi(this);
+
     ui->label->setStyleSheet("color : Black");
     ui->label_2->setStyleSheet("color : Black");
     ui->label_3->setStyleSheet("color : Black");
